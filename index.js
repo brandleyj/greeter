a5c1c4c1bef6e4c6aa2de6cf36b8e8069f66f525
Justin Brandley <brandleyj@gmail.com>
2019-10-31 12:22:29 -0500

    deploy commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const server = express(); // creating an instance of an express server
 
-server.get('/', (req, res) => {
-  res.status(200).json({ hello: 'Web 23' });
+server.get("/", (req, res) => {
+	res.status(200).json({ hello: "Web 23" });
 });
 
-server.listen(4000, () => {
-  console.log('\n Server running on port 4000 \n');
+// the port is coming from the environment
+const port = process.env.PORT || 4000;
+//the PORT will be set by heroku when we deploy
+
+server.listen(port, () => {
+	console.log("\n Server running on port 4000 \n");
 });
