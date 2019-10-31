1edfa7686ef254a27b6e0d03c7854587d3597e2d
Justin Brandley <brandleyj@gmail.com>
2019-10-31 12:50:12 -0500

    added dotenv

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require("dotenv").config;
+
 const express = require("express");
+
+console.log("\nmessage:", process.env.MSG);
 
 const server = express(); // creating an instance of an express server
 
 server.get("/", (req, res) => {
-	res.status(200).json({ hello: "Web 23" });
+	res.status(200).json({ message: process.env.MSG });
 });
 
 // the port is coming from the environment
